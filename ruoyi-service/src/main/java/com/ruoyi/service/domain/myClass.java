5183ea81fdfea90c1f11b1dd5b56288bb4868f45
zuk <123123@qq.com>
2021-04-14 14:37:22 +0800

    班级1.0

--- ruoyi-service/src/main/java/com/ruoyi/service/domain/myClass.java
+++ ruoyi-service/src/main/java/com/ruoyi/service/domain/myClass.java
@@ -9,7 +9,7 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 班级管理对象 class
- * 
+ *
  * @author XD
  * @date 2021-04-09
  */
@@ -28,6 +28,52 @@ public class myClass extends BaseEntity
     @Excel(name = "外键专业ID")
     private Long majorId;
 
+    private String universityName;
+    private String collegeName;
+    private String majorName;
+    private Long universityId;
+    private Long collegeId;
+
+    public Long getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(Long universityId) {
+        this.universityId = universityId;
+    }
+
+    public Long getCollegeId() {
+        return collegeId;
+    }
+
+    public void setCollegeId(Long collegeId) {
+        this.collegeId = collegeId;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getMajorName() {
+        return majorName;
+    }
+
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+
     /** 创建时间 */
     @JsonFormat(pattern = "yyyy-MM-dd")
     @Excel(name = "创建时间", width = 30, dateFormat = "yyyy-MM-dd")
@@ -41,57 +87,57 @@ public class myClass extends BaseEntity
     @Excel(name = "备注")
     private String remarks;
 
-    public void setClassId(Long classId) 
+    public void setClassId(Long classId)
     {
         this.classId = classId;
     }
 
-    public Long getClassId() 
+    public Long getClassId()
     {
         return classId;
     }
-    public void setClassName(String className) 
+    public void setClassName(String className)
     {
         this.className = className;
     }
 
-    public String getClassName() 
+    public String getClassName()
     {
         return className;
     }
-    public void setMajorId(Long majorId) 
+    public void setMajorId(Long majorId)
     {
         this.majorId = majorId;
     }
 
-    public Long getMajorId() 
+    public Long getMajorId()
     {
         return majorId;
     }
-    public void setCreateDate(Date createDate) 
+    public void setCreateDate(Date createDate)
     {
         this.createDate = createDate;
     }
 
-    public Date getCreateDate() 
+    public Date getCreateDate()
     {
         return createDate;
     }
-    public void setState(Long state) 
+    public void setState(Long state)
     {
         this.state = state;
     }
 
-    public Long getState() 
+    public Long getState()
     {
         return state;
     }
-    public void setRemarks(String remarks) 
+    public void setRemarks(String remarks)
     {
         this.remarks = remarks;
     }
 
-    public String getRemarks() 
+    public String getRemarks()
     {
         return remarks;
     }
@@ -101,7 +147,12 @@ public class myClass extends BaseEntity
         return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
             .append("classId", getClassId())
             .append("className", getClassName())
+            .append("universityId", getUniversityId())
+            .append("universityName", getUniversityName())
+            .append("collegeId", getCollegeId())
+            .append("collegeName", getCollegeName())
             .append("majorId", getMajorId())
+            .append("majorName", getMajorName())
             .append("createDate", getCreateDate())
             .append("state", getState())
             .append("remarks", getRemarks())
